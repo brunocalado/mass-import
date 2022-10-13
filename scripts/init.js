9763e289d3f234aa122c9ba169981bf7715860ac
Bruno Calado <brunocalado@users.noreply.github.com>
2022-10-13 15:56:19 -0300

    0.0.1

--- scripts/init.js
+++ scripts/init.js
@@ -1,12 +1,15 @@
 const moduleName = 'mass-import';
-import { sceneImporter } from './scene-importer.js'
+
 import { deckImporter } from './deck-importer.js'
+import { journalImporter } from './journal-importer.js'
+import { sceneImporter } from './scene-importer.js'
 
 Hooks.once('init', () => {
   // --------------------------------------------------
   // Load API
-  game.modules.get(moduleName).scene = { sceneImporter }; // Request with: const sceneImporter = game.modules.get('mass-import')?.scene.sceneImporter;
   game.modules.get(moduleName).deck = { deckImporter }; // Request with: const deckImporter = game.modules.get('mass-import')?.deck.deckImporter;
+  game.modules.get(moduleName).journal = { journalImporter }; // Request with: const journalImporter = game.modules.get('mass-import')?.journal.journalImporter;
+  game.modules.get(moduleName).scene = { sceneImporter }; // Request with: const sceneImporter = game.modules.get('mass-import')?.scene.sceneImporter;
 
   // --------------------------------------------------
   // SETTINGS
---
+++ scripts/journal-importer.js
@@ -0,0 +1,135 @@
+import { common } from './common.js'
+
+export class journalImporter {
+
+  // ---------------------------------------------------------
+  //
+  static async imageToJournal() {
+   
+    const basePath = 'modules/mass-import/WORKSPACE/taroticum'; // 'modules/mymaps/animatedmaps'
+    
+    const templateData = {basePath: basePath}; 
+    const dialogTemplate = await renderTemplate( `modules/mass-import/templates/image-to-journal-dialog.html`, templateData );                
+    
+    new Dialog({
+      title: `Folder To Journal`,
+      content: dialogTemplate,
+      buttons: {
+        roll: {
+          label: "Create",
+          callback: (html) => {
+            this.createJournal(html);
+          }
+        }, 
+        cancel: {
+          label: "Cancel"
+        }
+      },
+      render: (html) => listener(html)
+    }).render(true);
+
+    function listener(html) {
+      html.find(".picker-button").on("click", function() {
+        new FilePicker({
+          type: "folder",
+          callback: function (path) {
+            html.find("input[name=folder-path]").val(path);
+        }}).render(true);
+      });
+    }    
+    
+  }
+  
+  // ---------------------------------------------------------
+  //  
+  static async createJournal(html) {
+    const folderName = html.find("input[id=folder_name").val();  
+    const journalName = html.find("input[id=journal_name").val();  
+    const folderPath = html.find("input[name=folder-path]").val();  
+    const importType = parseInt( html.find("select[name=select_import_type]").val() );  
+    let journalData = {};
+
+    // Split files
+    let {files} = await FilePicker.browse("data", folderPath);
+    
+    // Folder
+    const createdFolder = await Folder.createDocuments([{name: folderName, type: "JournalEntry"}]);
+    const folderID = createdFolder[0].id;    
+    
+    // Data
+    journalData.journalName = journalName;
+    journalData.folderID = folderID;
+    
+    /* Import Types
+    0 - Each Image One Page
+    1 - All Images into Text Page
+    */
+    switch(importType) {
+      case 0:
+        this.oneImageOnePage(files, journalData);
+        break;
+      case 1:
+        this.allImagesToOneTextPage(files, journalData);        
+        break;   
+      default:
+        // code block
+    }    
+    
+  }   
+  
+  // --------------------------------
+  // Functions
+  // ---------------------------------------------------------
+  
+  // This will create one journal with one image page for each image.
+  static async oneImageOnePage(files, data) {
+    let images = [];
+    for (let imagePath of files) {
+      images.push({
+        "name": common.splitPath(imagePath),
+        "type": "image",
+        "src": imagePath,
+        "title": {
+          "show": false
+        }
+      })
+    }    
+
+    await JournalEntry.create({
+      name: data.journalName,
+      folder: data.folderID,
+      pages: images      
+    });    
+  } // END oneImageOnePage
+
+  // This will create one journal with one text page with all images.
+  static async allImagesToOneTextPage(files, data) {
+    let images = ``;
+    let pages = [];
+    for (let imagePath of files) {
+      images = images + `<img src=\"${imagePath}\" />`;
+    }    
+    
+    pages.push(
+      {
+        "name": 'My Images',
+        "type": "text",
+        "title": {
+          "show": false
+        },
+        "text": {
+          "content": images
+        },        
+      }    
+    );
+    
+    await JournalEntry.create({
+      name: data.journalName,
+      folder: data.folderID,
+      pages: pages      
+    });    
+  } // END oneImageOnePage
+    
+} // END CLASS
+
+
